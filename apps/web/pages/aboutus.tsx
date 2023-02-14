659e9e4334a85f2608e950e87a40c1d351a99116
mesmerize86 <lasataya@gmail.com>
2023-02-15 09:40:31 +1100

    develop branch compute

--- apps/web/pages/aboutus.tsx
+++ apps/web/pages/aboutus.tsx
@@ -1,6 +1,9 @@
 import { Button } from "ui";
 
-export default function Web() {
+export default function About() {
+    for(var i = 0; i < 10; i++){
+        console.log('i', i);
+    }
   return (
     <div>
       <h1>Web</h1>
