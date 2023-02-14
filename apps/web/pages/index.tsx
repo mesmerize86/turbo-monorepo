2bf8b24a879651fb419eb2ce6933643efa869ca5
kishorfreely <kishor@freely.me>
2023-02-14 15:12:56 +1100

    feature branch test

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-      <h1>Git flow feature branch</h1>
+      <h1>Git flow feature branch test</h1>
       <Button />
     </div>
   );
