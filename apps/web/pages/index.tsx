3d984af4e8d496f77da9a8f7edab9049eb6c380a
kishorfreely <kishor@freely.me>
2023-02-21 10:51:21 +1100

    test branch anaylysis

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -2,6 +2,9 @@ import { Button } from "ui";
 
 export default function Web() {
   const test = ' test ';
+  for(var i = 0; i < = 10; i++) {
+    console.log(i);
+  }
   return (
     <div>
       <h1>Git flow feature branch test</h1>
