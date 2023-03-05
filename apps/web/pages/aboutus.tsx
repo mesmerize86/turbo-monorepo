184c89542bf9619351bb922af6fffae00cdb3654
kishorfreely <kishor@freely.me>
2023-03-06 09:30:52 +1100

    undefined

--- apps/web/pages/aboutus.tsx
+++ apps/web/pages/aboutus.tsx
@@ -2,7 +2,7 @@ import { Button } from "ui";
 
 export default function About() {
 
-  const returnTypeFunction = (param1:boolean, param2:boolean)=> {
+  const returnTypeFunction = (param1:boolean, param2:boolean): string | undefined=> {
     if(param1 && param2){
       return 'param1 and param2 are true'
     }
