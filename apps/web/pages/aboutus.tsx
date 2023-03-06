e61b759ae055e22dedf3779f33650153f34c6073
kishorfreely <kishor@freely.me>
2023-03-07 09:42:18 +1100

    age boolean

--- apps/web/pages/aboutus.tsx
+++ apps/web/pages/aboutus.tsx
@@ -7,15 +7,19 @@ export default function About() {
       return 'param1 and param2 are true'
     }
   }
+  const age = 5;
 
   returnTypeFunction(true , true);
   console.log('return string ', returnTypeFunction(true , true));
   console.log('return undefined', returnTypeFunction(true , false));
 
   return (
+    <>
     <div>
       <h1>Web</h1>
       <Button />
     </div>
+    {age && {age}}
+    </>
   );
 }
